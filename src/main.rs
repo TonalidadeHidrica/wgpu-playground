0b3142b08fcb86c516f4a75f6bf15804bd738c83
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2021-12-30 00:58:10 +0900

    Create a surface and handle resize

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::io::BufWriter;
 
+use anyhow::Context;
 use fs_err::OpenOptions;
+use pollster::block_on;
 use simplelog::{CombinedLogger, LevelFilter, TermLogger, WriteLogger};
+use wgpu::{
+    Backends, DeviceDescriptor, RequestAdapterOptions, SurfaceConfiguration, TextureUsages,
+};
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
@@ -11,6 +16,7 @@ use winit::{
 #[allow(clippy::single_match)]
 #[allow(clippy::collapsible_match)]
 fn main() -> anyhow::Result<()> {
+    // Logging
     CombinedLogger::init(vec![
         TermLogger::new(
             LevelFilter::Warn,
@@ -30,14 +36,60 @@ fn main() -> anyhow::Result<()> {
         ),
     ])?;
 
+    // winit
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop)?;
+
+    // wgpu
+    let size = window.inner_size();
+    let instance = wgpu::Instance::new(Backends::all());
+    // SAFETY: Window handle is valid (except for Android) and will live shorter than window
+    // In Android, we should first wait for "Resumed" event (see docs of `impl HasRawWindowHandle
+    // for Window`)
+    let surface = unsafe { instance.create_surface(&window) };
+    // This is a handle to our actual graphics card
+    let adapter = block_on(instance.request_adapter(&RequestAdapterOptions {
+        power_preference: Default::default(),
+        compatible_surface: Some(&surface),
+        force_fallback_adapter: false,
+    }))
+    .context("Failed to request an adapter")?;
+    // Connects a physical device to create a logical device
+    let (device, _queue) = block_on(adapter.request_device(
+        &DeviceDescriptor {
+            label: None,
+            features: wgpu::Features::empty(),
+            limits: Default::default(),
+        },
+        None,
+    ))
+    .context("Failed to request a device")?;
+    let mut config = SurfaceConfiguration {
+        usage: TextureUsages::RENDER_ATTACHMENT,
+        format: surface
+            .get_preferred_format(&adapter)
+            .context("Failed to get preferred format")?,
+        width: size.width.max(1),
+        height: size.height.max(1),
+        present_mode: wgpu::PresentMode::Fifo,
+    };
+    surface.configure(&device, &config);
 
     event_loop.run(move |event, _, control_flow| match event {
         Event::WindowEvent {
             window_id, event, ..
         } if window_id == window.id() => match event {
             WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+            WindowEvent::Resized(size)
+            | WindowEvent::ScaleFactorChanged {
+                new_inner_size: &mut size,
+                ..
+            } => {
+                dbg!(size);
+                config.width = size.width.max(1);
+                config.height = size.height.max(1);
+                surface.configure(&device, &config);
+            }
             _ => {}
         },
         _ => {}
